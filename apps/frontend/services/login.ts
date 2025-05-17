b91637bfb00ced7b12ede6693b457ea08b3eb347
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2025-05-17 23:14:51 +0530

    add headers to login service

--- apps/frontend/services/login.ts
+++ apps/frontend/services/login.ts
@@ -1,7 +1,13 @@
 import type { LoginInputUserDTO } from "@repo/types";
 import axios from "axios";
 import { apiBaseUrl } from "./config";
-export const login = async (loginData: LoginInputUserDTO) => {
-  const response = await axios.post(`${apiBaseUrl}/auth/login`);
+
+export const login = async (data: LoginInputUserDTO) => {
+  const response = await axios.post(`${apiBaseUrl}/auth/login`, data, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+  });
   return response.data;
 };
