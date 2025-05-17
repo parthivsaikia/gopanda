fc1c30ab5dbf640695fdb962635b425b106ddb8b
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2025-05-17 21:23:46 +0530

    add clientAction in login page

--- apps/frontend/app/routes/login.tsx
+++ apps/frontend/app/routes/login.tsx
@@ -1,4 +1,10 @@
 import { Form } from "react-router";
+import type { Route } from "./+types/login";
+
+export async function clientAction({ request }: Route.ClientActionArgs) {
+  const formData = await request.formData();
+  const loginData = Object.fromEntries(formData);
+}
 
 export default function LoginPage() {
   return (
@@ -8,6 +14,11 @@ export default function LoginPage() {
           <label htmlFor="username">Username</label>
           <input type="text" name="username" id="username" />
         </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+        </div>
+        <button type="submit">Log in</button>
       </Form>
     </div>
   );
