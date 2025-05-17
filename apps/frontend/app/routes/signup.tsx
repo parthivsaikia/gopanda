550410ee3d1efc3344dffb6344af49ace574d6d5
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2025-05-17 13:28:02 +0530

    add clientAction to signup page

--- apps/frontend/app/routes/signup.tsx
+++ apps/frontend/app/routes/signup.tsx
@@ -1,9 +1,17 @@
 import { Form } from "react-router";
+import type { Route } from "./+types/signup";
+import { LoginInputUserDTOSchema } from "@repo/types";
+
+export async function clientAction({ request }: Route.ClientActionArgs) {
+  const formData = await request.formData();
+  const signupData = Object.fromEntries(formData);
+  console.log(signupData);
+}
 
 export default function SignupPage() {
   return (
     <div>
-      <Form>
+      <Form method="post">
         <div>
           <label htmlFor="name">Name</label>
           <input type="text" name="name" id="name" />
@@ -15,24 +23,25 @@ export default function SignupPage() {
         <div>
           <label htmlFor="password">Password</label>
           <input type="password" name="password" id="password" />
-        </div>{" "}
+        </div>
         <div>
           <label htmlFor="email">Email</label>
           <input type="email" name="email" id="email" />
-        </div>{" "}
+        </div>
         <div>
           <label htmlFor="mobileNumber">Mobile Number</label>
           <input type="tel" name="mobileNumber" id="mobileNumber" />
-        </div>{" "}
+        </div>
         <div>
           <label htmlFor="state">State</label>
           <input type="text" name="state" id="state" />
-        </div>{" "}
+        </div>
         <div>
           <label htmlFor="country">Country</label>
           <input type="text" name="country" id="country" />
-        </div>{" "}
+        </div>
         <div>
+          <label>Role</label>
           <div>
             <input type="radio" name="role" id="customer" value={`Customer`} />
             <label htmlFor="customer">Customer</label>
@@ -47,6 +56,7 @@ export default function SignupPage() {
             <label htmlFor="travelAgent">Travel Agent</label>
           </div>
         </div>
+        <button type="submit">Submit</button>
       </Form>
     </div>
   );
