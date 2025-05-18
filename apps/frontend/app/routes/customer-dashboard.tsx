3d9f4c4ae9a2a7db7d3d8593ff7388fc24a73b70
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2025-05-18 20:03:28 +0530

    add customer-dashboard page

--- apps/frontend/app/routes/customer-dashboard.tsx
+++ apps/frontend/app/routes/customer-dashboard.tsx
@@ -1,1 +1,25 @@
-import { Form } from "react-router";
+import type { Route } from "./+types/customer-dashboard";
+import { getProfile } from "services/profile";
+import { Link } from "react-router";
+
+export async function clientLoader() {
+  const profileData = await getProfile();
+  console.log(profileData);
+  return profileData;
+}
+
+export default function CustomerDashboard({
+  loaderData,
+}: Route.ComponentProps) {
+  const userData = loaderData;
+  return (
+    <div>
+      <p>{userData.name}</p>
+      <p>{userData.username}</p>
+      <p>{userData.state}</p>
+      <p>{userData.country}</p>
+      <p>{userData.role}</p>
+      <Link to={`/logout`}>Log out</Link>
+    </div>
+  );
+}
