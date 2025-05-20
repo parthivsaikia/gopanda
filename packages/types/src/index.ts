7c4c6f5100d3ce088c457e9acba8464b5d5e7e86
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2025-05-20 08:54:11 +0530

    export tourtypes and schemas

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,3 +1,5 @@
 export type { User, Session, UserRole } from "./prisma-types.js";
 export * from "./userTypes/schema.js";
 export * from "./userTypes/types.js";
+export * from "./tourTypes/schema.js";
+export * from "./tourTypes/types.js";
